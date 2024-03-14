d5021e3457cd5eb773c84945de724b820cbdc007
Anees Haroon <aharoon1007@gmail.com>
2024-03-14 21:42:44 +0000

    Add scroll progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 //@ts-ignore
 import { Navbar, Hero, About, Projects } from "./components"
+import { motion, useScroll, useSpring } from "framer-motion"
 
 const App = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <div>
+      <motion.div className="fixed top-0 left-0 right-0 h-1 z-20 bg-[#9845F5] transform origin-left" style={{ scaleX }} />
       <Navbar />
       <Hero />
       <About />
