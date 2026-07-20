7470cbf8aa50f95abfc46da32ad787f46e462763
Anees <aharoon1007@gmail.com>
2026-07-20 17:12:52 +0100

    Remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 //@ts-ignore
 import { Navbar, Hero, About, Projects, Timeline } from "./components";
-import { motion, useScroll, useSpring } from "framer-motion";
 
 const App = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 200,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
   return (
     <div>
       <Navbar />
